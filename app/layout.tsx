b40fd7c31a3973726fcadc97d61c1722c90aba51
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-03-31 08:33:18 -0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+"use client"
+import "../styles/globals.css"
+import { ThemeProvider } from "next-themes"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className="dark:bg-stone-900">
+        
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import HeroSection from "@/components/HeroSection"
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <HeroSection />
+    </main>
+  )
+}
